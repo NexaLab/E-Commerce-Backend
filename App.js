d2067025f11fedc68d1c52d8f70d66bb20218b06
Abdur Rafay <abdulrafey055@gmail.com>
2023-03-18 00:37:02 +0500

    Backend Test

---
+++ App.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require('body-parser');
+
+
+require('dotenv').config()
+
+//    **********      Database Connected         **********
+
+
+
+
+require("./DbConnection/DbConnection");
+
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+
+
+app.use(cors({
+    origin: '*' // allow requests from this domain
+  }));
+
+
+
+
+
+
+  app.get( "/api/hello" , ( req , res ) => {
+
+    res.send("App Running");
+
+})
+
+
+
+
+
+const port = process.env.PORT || 3001;
+
+app.listen( port , () => {
+
+    console.log("App listening on port 3001");
+
+} );
